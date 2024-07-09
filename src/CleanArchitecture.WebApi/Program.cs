62db363b07f0a805fa7d9f7c7b460d5605253853
Ahmet Emin Öztürk <eminahmetozturk@gmail.com>
2024-07-09 17:41:26 +0300

    GenericFluentValidation

--- src/CleanArchitecture.WebApi/Program.cs
+++ src/CleanArchitecture.WebApi/Program.cs
@@ -1,8 +1,15 @@
+using CleanArchitecture.Application.Behaviors;
+using CleanArchitecture.Application.Services;
 using CleanArchitecture.Persistance.Context;
+using CleanArchitecture.Persistance.Services;
+using FluentValidation;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<ICarService, CarService>();
+builder.Services.AddAutoMapper(typeof(CleanArchitecture.Persistance.AssemblyReference).Assembly);
 
 string connectionString = builder.Configuration.GetConnectionString("SqlServer");
 
@@ -10,6 +17,12 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(conn
 
 builder.Services.AddControllers()
                 .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+
+builder.Services.AddMediatR(cfr =>
+             cfr.RegisterServicesFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly));
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+builder.Services.AddValidatorsFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ src/Core/CleanArchitecture.Application/Features/CarFeatures/Commands/CreateCar/CreateCarCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar
+{
+    public sealed class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, MessageResponse>
+    {
+        private readonly ICarService _carService;
+
+        public CreateCarCommandHandler(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        public async Task<MessageResponse> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            await _carService.CreateAsync(request, cancellationToken);
+            return new("Araç kayıt başarılı.");
+        }
+    }
+}

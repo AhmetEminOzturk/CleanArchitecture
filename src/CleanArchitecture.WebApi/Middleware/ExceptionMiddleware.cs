b04d628c974f989e20020f5a83d5534a71849a61
Ahmet Emin Öztürk <eminahmetozturk@gmail.com>
2024-07-10 12:48:45 +0300

    Error Log

--- src/CleanArchitecture.WebApi/Middleware/ExceptionMiddleware.cs
+++ src/CleanArchitecture.WebApi/Middleware/ExceptionMiddleware.cs
@@ -1,9 +1,18 @@
-﻿using FluentValidation;
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Persistance.Context;
+using FluentValidation;
 
 namespace CleanArchitecture.WebApi.Middleware;
 
 public sealed class ExceptionMiddleware : IMiddleware
 {
+    private readonly AppDbContext _context;
+
+    public ExceptionMiddleware(AppDbContext context)
+    {
+        _context = context;
+    }
+
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
 			try
@@ -12,6 +21,7 @@ public sealed class ExceptionMiddleware : IMiddleware
 			}
 			catch (Exception ex)
         {
+            await LogExceptionToDatabaseAsync(ex, context.Request);
             await HandleExceptionAsync(context, ex);
         }
     }
@@ -35,5 +45,20 @@ public sealed class ExceptionMiddleware : IMiddleware
             Message = ex.Message,
             StatusCode = context.Response.StatusCode
         }.ToString());
+    }
+
+    private async Task LogExceptionToDatabaseAsync(Exception ex, HttpRequest request)
+    {
+        ErrorLog errorLog = new()
+        {
+            ErrorMessage = ex.Message,
+            StackTrace = ex.StackTrace,
+            RequestPath = request.Path,
+            RequestMethod = request.Method,
+            Timestamp = DateTime.Now,
+        };
+
+        await _context.Set<ErrorLog>().AddAsync(errorLog, default);
+        await _context.SaveChangesAsync(default);
     }
 }
---
+++ src/External/CleanArchitecture.Persistance/Migrations/20240710093410_mig2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateDate",
+                table: "Cars",
+                newName: "CreatedDate");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "ErrorLogs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ErrorMessage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StackTrace = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RequestPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RequestMethod = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ErrorLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ErrorLogs");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Cars",
+                newName: "CreateDate");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
